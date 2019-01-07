0ea72ae3d353b2ac0b1aa961218bbf7770aabd41
NauGaika <cklyarenkovlad@gmail.com>
2019-01-07 19:55:53 +0300

    change pass db

--- server/__init__.py
+++ server/__init__.py
@@ -7,7 +7,7 @@ from flask_migrate import Migrate, MigrateCommand
 basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
 basedir = os.path.join(basedir, 'server.db')
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1111@localhost/sev_steklo'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:5374fa7e@localhost/sevglass'
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
