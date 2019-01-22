21eddd8707ecf1409735d802be433ab695e258e7
NauGaika <cklyarenkovlad@gmail.com>
2019-01-22 13:41:43 +0300

    добавил гитигнор

--- server/__init__.py
+++ server/__init__.py
@@ -2,19 +2,15 @@
 from flask import Flask
 import os
 import sys
-# from .conf import Config
+from .conf import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
-basedir = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
-basedir = os.path.join(basedir, 'server.db')
+
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:5374fa7e@localhost/sevsteklo'
-app.config['SQLALCHEMY_ECHO'] = False
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['UPLOAD_FOLDER'] = "../nuxt_app/static/img/articles/"
-
+app.config.from_object(Config)
 db = SQLAlchemy(app)
-
 migrate = Migrate(app, db)
+
 
 from . import models, routes
--- server/conf.py
+++ server/conf.py
@@ -3,6 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'server.db')
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1111@localhost/sev_steklo'
+    SQLALCHEMY_ECHO = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    UPLOAD_FOLDER = "../nuxt_app/static/img/articles/"
